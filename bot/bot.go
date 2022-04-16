8200af323d43eca8303f601c1f8279b3fd74e879
Dmitry Zhmurko <zhmurko@gmail.com>
2022-04-16 21:12:08 +0300

    prefix indent

--- bot/bot.go
+++ bot/bot.go
@@ -13,7 +13,7 @@ import (
 
 func prettyprint(b []byte) ([]byte, error) {
     var out bytes.Buffer
-    err := json.Indent(&out, b, "", "  ")
+    err := json.Indent(&out, b, "|", "  ")
     return out.Bytes(), err
 }
 
