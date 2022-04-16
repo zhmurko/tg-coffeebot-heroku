816d9cecd584dcee240b49f7b41102465e7971f0
Dmitry Zhmurko <zhmurko@gmail.com>
2022-04-16 20:38:45 +0300

    register webhook

--- main.go
+++ main.go
@@ -1,31 +1,31 @@
 package main
 
 import (
-    "github.com/zhmurko/tg-coffeebot-heroku/bot"
-    "log"
-    //"net/http"
-    "os"
-    "github.com/gin-gonic/gin"
-    _ "github.com/heroku/x/hmetrics/onload"
+	"github.com/zhmurko/tg-coffeebot-heroku/bot"
+	"log"
+	//"net/http"
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+	"os"
+	"sync"
 )
 
+var doOnce sync.Once
+
 func main() {
-    port := os.Getenv("PORT")
+	port := os.Getenv("PORT")
 
-    token, ok := os.LookupEnv("BOT_TOKEN")
-    if !ok {
-        token = "empty"
-    }    
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 
-    telegramUrl := "https://api.telegram.org/bot" + token;
-    _ = telegramUrl
-    if port == "" {
-            log.Fatal("$PORT must be set")
-    }
-    
-    router := gin.New()
-    router.Use(gin.Logger())
-    router.GET("/ping", bot.Pong)
-    
-    router.Run(":" + port)
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.GET("/ping", bot.Pong)
+
+	doOnce.Do(func() {
+		bot.RegisterWebhook()
+	})
+	router.Run(":" + port)
+
 }
