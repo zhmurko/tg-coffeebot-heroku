4e781ff5fea37d6c04266e7e95f3a00c1b63eacf
Dmitry Zhmurko <zhmurko@gmail.com>
2022-04-17 19:30:26 +0300

    dump callback_query

--- bot/bot.go
+++ bot/bot.go
@@ -8,20 +8,20 @@ import (
 )
 
 func Respond(c *gin.Context) {
-	dumpPost(c)
-	// var chat Update
-	// err := c.ShouldBindJSON(&chat)
-	// if err != nil {
-	// 	log.Println(err)
-	// }
-	// id := chat.Message.Chat.Id
-	// log.Printf("ID: %d", id)
-	// log.Printf("R: %+v", chat)
-	// if chat.Message.Text == "/menu" {
-	// 	ReplyMenu(id)
-	// } else {
-	// 	SendText(id, "type /menu")
-	// }
+	// dumpPost(c)
+	var chat Update
+	err := c.ShouldBindJSON(&chat)
+	if err != nil {
+		log.Println(err)
+	}
+	id := chat.Message.Chat.Id
+	log.Printf("ID: %d", id)
+	log.Printf("R: %+v", chat)
+	if chat.Message.Text == "/menu" {
+		ReplyMenu(id)
+	} else {
+		SendText(id, "type /menu")
+	}
 }
 
 func Pong(c *gin.Context) {
--- bot/respond.go
+++ bot/respond.go
@@ -54,3 +54,52 @@ type Update struct {
 // |    "text": "test"
 // |  }
 // |}
+
+// Respond with callback_query
+// |{
+// |  "update_id": 841266275,
+// |  "callback_query": {
+// |    "id": "1005626476713560102",
+// |    "from": {
+// |      "id": 234140659,
+// |      "is_bot": false,
+// |      "first_name": "Dmitry",
+// |      "username": "zhmurko",
+// |      "language_code": "en"
+// |    },
+// |    "message": {
+// |      "message_id": 713,
+// |      "from": {
+// |        "id": 5176662516,
+// |        "is_bot": true,
+// |        "first_name": "Coffee, please",
+// |        "username": "OneCoffeebot"
+// |      },
+// |      "chat": {
+// |        "id": 234140659,
+// |        "first_name": "Dmitry",
+// |        "username": "zhmurko",
+// |        "type": "private"
+// |      },
+// |      "date": 1650212725,
+// |      "text": "Select:",
+// |      "reply_markup": {
+// |        "inline_keyboard": [
+// |          [
+// |            {
+// |              "text": "Espresso",
+// |              "callback_data": "order:Espresso"
+// |            },
+// |            {
+//               "text": "Latte",
+// |              "callback_data": "order:Latte"
+// |            }
+// |          ]
+// |        ]
+// |      }
+// |    },
+// |    "chat_instance": "-2656029114737977985",
+// |    "data": "order:Espresso"
+// |  }
+// |}
+// 
