8736b12f7f857fcebfde1be70d23f64c02076391
Dmitry Zhmurko <zhmurko@gmail.com>
2022-04-23 17:16:56 +0300

    fix who int

--- bot/bot.go
+++ bot/bot.go
@@ -31,14 +31,16 @@ func Respond(c *gin.Context) {
 		var coffee string
 		_, _ = fmt.Sscanf(text, "order:%s", &coffee)
 		ReplyOrder(adminId, coffee, fmt.Sprint(id))
-		SendText(id, "Doing "+coffee+ "for you")
+		SendText(id, "Doing "+coffee+" for you")
 	case strings.HasPrefix(text, "ready:"):
 		var coffee string
 		var who int
-		_, _ = fmt.Sscanf(text, "ready:%s:%d", &coffee, &who)
+		order := strings.Split(text, ":")
+		coffee = order[1]
+		_, _ = fmt.Sscanf(order[2], "%d", &who)
 		message_id := chat.Message.Id + chat.CallbackQuery.Message.Id
 		DeleteMessage(adminId, message_id)
-		SendText(adminId, "Completed " +coffee+ " for " + fmt.Sprint(who))
+		SendText(adminId, "Completed "+coffee+" for "+fmt.Sprint(who))
 		SendText(who, "Your "+coffee+" is ready")
 	case strings.HasPrefix(text, "/"):
 		switch text {
