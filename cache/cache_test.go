35ada84099b461c8b901428bdeefb4963b7fec60
Dmitry Zhmurko <zhmurko@gmail.com>
2022-05-01 10:31:31 +0300

    Test cache instance

---
+++ cache/cache_test.go
@@ -0,0 +1,48 @@
+package cache_test
+
+import (
+	"testing"
+
+	_ "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+
+	// "github.com/memcachier/mc/v3"
+	cacheHandler "github.com/zhmurko/tg-coffeebot-heroku/cache"
+)
+
+type CacheSuite struct {
+	suite.Suite
+	// Cache *mc.Client
+}
+
+func (suite *CacheSuite) SetupSuite() {
+	cacheHandler.Cache = cacheHandler.Register()
+}
+
+func (suite *CacheSuite) TearDownSuite() {
+	cacheHandler.Cache.Quit()
+}
+
+func (suite *CacheSuite) TestRegister() {
+	require.NotNil(suite.T(), cacheHandler.Cache)
+
+	version, err := cacheHandler.Cache.Version()
+	require.NoError(suite.T(), err)
+	server := map[string]string{"localhost:11211": "1.6.15"}
+	require.Equal(suite.T(), version, server)
+}
+
+func (suite *CacheSuite) TestMemory() {
+	cacheHandler.RememberMe("1", "username")
+	name := cacheHandler.WhatsMyName("1")
+	require.Equal(suite.T(), name, "username")
+}
+
+// 'go test' starts here
+func TestCache(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skip integration test for cache")
+	}
+	suite.Run(t, new(CacheSuite))
+}
--- go.mod
+++ go.mod
@@ -9,9 +9,11 @@ require (
 	github.com/heroku/x v0.0.50
 	github.com/lib/pq v1.10.5
 	github.com/memcachier/mc/v3 v3.0.3
+	github.com/stretchr/testify v1.7.0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.13.0 // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
@@ -24,10 +26,13 @@ require (
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/sys v0.0.0-20220317061510-51cd9980dadf // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	rsc.io/pdf v0.1.1 // indirect
 )
